d69c822e1d3fe06ffe12a2f28ff9a70e2758cd1f
Hamad Abdullah <95331251+HamadAbdullah2001@users.noreply.github.com>
2022-11-18 18:43:28 +0300

    Add print statement to print my name

--- Project2/bikeshare_2.py
+++ Project2/bikeshare_2.py
@@ -6,6 +6,8 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+print("My name is hamad")
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
